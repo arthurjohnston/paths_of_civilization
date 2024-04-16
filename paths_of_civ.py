61c8f7c5a60507caa0eb0b5aee7ea843969384ca
arthur <arthur.j.johnston@gmail.com>
2024-04-15 19:21:56 -0700

    hard coded the hand combos as a work around as itertools doesn't handle dupes the way this needs

--- paths_of_civ.py
+++ paths_of_civ.py
@@ -1,22 +1,64 @@
 import itertools
 from cards import *
+import datetime
+import cProfile
 
-def get_card_combinations(cards):
+# fix this to handle duplicate cards properly, currently itertools treats them as the same
+def get_card_combinations_old(cards):
+    if(len(cards)!=5):
+        raise Exception(str(cards))
     # Generate all combinations of choosing 2 cards for the first group
     first_group_combinations = list(itertools.combinations(cards, 2))
 
     all_combinations = []
     for first_group in first_group_combinations:
-        remaining_cards = [card for card in cards if card not in first_group]
+        remaining_index = [card for card in cards if card not in first_group]
         # Generate all combinations of choosing 2 cards for the second group
-        second_group_combinations = list(itertools.combinations(remaining_cards, 2))
-        for second_group in second_group_combinations:
+        second_group = list(itertools.combinations(remaining_index, 2))
+        for second_group in second_group:
             # Generate all combinations of choosing 2 cards for the third group
-            third_group = [card for card in remaining_cards if card not in second_group]
+            third_group = [card for card in remaining_index if card not in second_group]
             all_combinations.append([first_group, second_group, third_group])
 
     return all_combinations
 
+def get_card_combinations(cards):
+    if len(cards) != 5:
+        raise ValueError("Input must be a list of 5 cards")
+    
+    all_combinations = set()
+    all_combinations.add(tuple([tuple([cards[0], cards[1]]),tuple([cards[2], cards[3]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[1]]),tuple([cards[2], cards[4]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[1]]),tuple([cards[3], cards[4]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[2]]),tuple([cards[1], cards[3]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[2]]),tuple([cards[1], cards[4]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[2]]),tuple([cards[3], cards[4]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[3]]),tuple([cards[1], cards[2]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[3]]),tuple([cards[1], cards[4]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[3]]),tuple([cards[2], cards[4]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[4]]),tuple([cards[1], cards[2]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[4]]),tuple([cards[1], cards[3]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[0], cards[4]]),tuple([cards[2], cards[3]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[2]]),tuple([cards[0], cards[3]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[2]]),tuple([cards[0], cards[4]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[2]]),tuple([cards[3], cards[4]]), tuple([cards[0]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[3]]),tuple([cards[0], cards[2]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[3]]),tuple([cards[0], cards[4]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[3]]),tuple([cards[2], cards[4]]), tuple([cards[0]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[4]]),tuple([cards[0], cards[2]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[4]]),tuple([cards[0], cards[3]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[1], cards[4]]),tuple([cards[2], cards[3]]), tuple([cards[0]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[3]]),tuple([cards[0], cards[1]]), tuple([cards[4]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[3]]),tuple([cards[0], cards[4]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[3]]),tuple([cards[1], cards[4]]), tuple([cards[0]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[4]]),tuple([cards[0], cards[1]]), tuple([cards[3]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[4]]),tuple([cards[0], cards[3]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[2], cards[4]]),tuple([cards[1], cards[3]]), tuple([cards[0]])]))
+    all_combinations.add(tuple([tuple([cards[3], cards[4]]),tuple([cards[0], cards[1]]), tuple([cards[2]])]))
+    all_combinations.add(tuple([tuple([cards[3], cards[4]]),tuple([cards[0], cards[2]]), tuple([cards[1]])]))
+    all_combinations.add(tuple([tuple([cards[3], cards[4]]),tuple([cards[1], cards[2]]), tuple([cards[0]])]))
+
+    return list(all_combinations)
 
 
 #    0:4,
@@ -108,70 +150,76 @@ class PlayerState:
 
 
 
-
-turn_1_hand = list(starting_cards.keys())
-turn_1_player_state =PlayerState(turn_1_hand) #todo add board specific bonus
-starts_of_turn = [turn_1_player_state]
-next_turn_starts=set()
-for turn in range(1,4):
-    for starting in starts_of_turn:
-        starting.events.append(f"Turn {turn}")
-        # step 1 generate all 30 card placements for this hand
-        combinations = get_card_combinations(starting.hand)
-        
-        card_placements = []
-        for left_group, right_group, discard in combinations:
-            card_placement=starting.deep_copy()
-            # step 2 remove discarded card
-            # todo this should save for final game scoring 
-            for card_name in discard:
-                card_placement.events.append("\tDiscard:"+str(card_name))
-                #card_placement.hand.remove(card)
-                card_placement.remove_card_from_hand(card_name)
+def runCode():
+    turn_1_hand = list(starting_cards.keys())
+    turn_1_player_state =PlayerState(turn_1_hand) #todo add board specific bonus
+    starts_of_turn = [turn_1_player_state]
+    next_turn_starts=set()
+    for turn in range(1,6):
+        for starting in starts_of_turn:
+            starting.events.append(f"Turn {turn}")
+            # step 1 generate all 30 card placements for this hand
+            combinations = get_card_combinations(starting.hand)[:10]
             
-            # step 3 add left resource
-            # todo add population check
-            for card_name in left_group:
-                card_placement.events.append("\tLeft:"+str(card_name))
-                for cube in playable_cards[card_name].left:
-                    card_placement.add_cube_amount(cube)
-            # Step 4 buy wonders & leaders
-        
-            # Step 5 add right resource
-            # todo add pop check
-            for card_name in right_group:
-                card_placement.events.append("\tRight:"+str(card_name))
-                for tech in playable_cards[card_name].right:
-                    card_placement.add_tech_amount(tech)
-            #step 6 buy card
-            #check which techs you have enough of
+            card_placements = []
+            for left_group, right_group, discard in combinations:
+                card_placement=starting.deep_copy()
+                # step 2 remove discarded card
+                # todo this should save for final game scoring 
+                if(len(discard)==0):
+                    raise Exception(starting.hand)
+                for card_name in discard:
+                    card_placement.events.append("\tDiscard:"+str(card_name))
+                    #card_placement.hand.remove(card)
+                    card_placement.remove_card_from_hand(card_name)
+                
+                # step 3 add left resource
+                # todo add population check
+                for card_name in left_group:
+                    card_placement.events.append("\tLeft:"+str(card_name))
+                    for cube in playable_cards[card_name].left:
+                        card_placement.add_cube_amount(cube)
+                # Step 4 buy wonders & leaders
             
-            for card_name, card in buyable_cards.items():
-                if card_placement.has_enought_tech(card.cost):
-                    #print("can afford card")
-                    next_turn_starting = card_placement.deep_copy()
-                    next_turn_starting.add_card_to_hand(card_name)
-                    next_turn_starting.events.append("\tBought:"+str(card_name))
-                    next_turn_starting.remove_tech_amount(card.cost)
-                    for bonus in  card.bonus:
-                        if isinstance(bonus, TechAmount):
-                            next_turn_starting.add_tech_amount(bonus)
-                        elif isinstance(bonus, CubeAmount):
-                            next_turn_starting.add_cube_amount(bonus)
-                        else:
-                            print("something bad"+bonus)
-                    #print(next_turn_starting)
-                    next_turn_starts.add(next_turn_starting)
-                    #print("---------")
-    print(f"At end of {turn} there are {len(next_turn_starts)} possible")
-        # copy over the starts for next turn and reset next_turn_starts to 
-        # be used for the following turn
-    starts_of_turn=next_turn_starts
-    next_turn_starts=set() #reset possibilities for next 
+                # Step 5 add right resource
+                # todo add pop check
+                for card_name in right_group:
+                    card_placement.events.append("\tRight:"+str(card_name))
+                    for tech in playable_cards[card_name].right:
+                        card_placement.add_tech_amount(tech)
+                #step 6 buy card
+                #check which techs you have enough of
+                
+                for card_name, card in buyable_cards.items():
+                    if card_placement.has_enought_tech(card.cost):
+                        #print("can afford card")
+                        next_turn_starting = card_placement.deep_copy()
+                        next_turn_starting.add_card_to_hand(card_name)
+                        next_turn_starting.events.append("\tBought:"+str(card_name))
+                        next_turn_starting.remove_tech_amount(card.cost)
+                        for bonus in  card.bonus:
+                            if isinstance(bonus, TechAmount):
+                                next_turn_starting.add_tech_amount(bonus)
+                            elif isinstance(bonus, CubeAmount):
+                                next_turn_starting.add_cube_amount(bonus)
+                            else:
+                                print("something bad"+bonus)
+                        #print(next_turn_starting)
+                        next_turn_starts.add(next_turn_starting)
+                        #print("---------")
+        print(datetime.datetime.now())
+        print(f"At end of {turn} there are {len(next_turn_starts)} possible")
+            # copy over the starts for next turn and reset next_turn_starts to 
+            # be used for the following turn
+        starts_of_turn=next_turn_starts
+        next_turn_starts=set() #reset possibilities for next 
+    
+    sorted_objects = sorted(starts_of_turn, key=lambda obj: obj.tech_score(), reverse=True)
+    
+    # Print the top ten objects
+    for i, obj in enumerate(sorted_objects[:5], start=1):
+        print(f"{obj} - \nTech Score: {obj.tech_score()}")
+        print("-----------------------------------")
 
-sorted_objects = sorted(starts_of_turn, key=lambda obj: obj.tech_score(), reverse=True)
-
-# Print the top ten objects
-for i, obj in enumerate(sorted_objects[:10], start=1):
-    print(f"{obj} - Tech Score: {obj.tech_score()}")
-    print("-----------------------------------")
+if __name__ == '__main__':
+    cProfile.run('runCode()')
